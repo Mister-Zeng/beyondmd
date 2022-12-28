404c84de4e720e4ed614a63135a6726fbe83dbf5
Jason <officialjasonzeng@gmail.com>
2022-12-27 19:41:31 -0500

    clean up

--- backend/beyondmd/serializers.py
+++ backend/beyondmd/serializers.py
@@ -1,8 +1,4 @@
-from abc import ABC
-
-from django.utils import timezone
 from rest_framework import serializers
-from rest_framework.response import Response
 
 from .models import Reviewer, Exercise
 
@@ -11,20 +7,6 @@ class ReviewerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reviewer
         fields = ['id', 'first_name', 'last_name', 'rating', 'comment', 'posted', 'exercise']
-
-    def create(self, validated_data):
-        return Reviewer.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.rating = validated_data.get('rating', instance.rating)
-        instance.comment = validated_data.get('comment', instance.comment)
-        instance.posted = validated_data.get('posted', instance.posted)
-        instance.exercise = validated_data.get('exercise', instance.exercise)
-        instance.save()
-        return instance
-
 
     @classmethod
     def get_reviewers_from_exercise(cls, exercise_id):
@@ -36,16 +18,3 @@ class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = ['id', 'name', 'exercise_type', 'muscle', 'equipment', 'difficulty', 'instructions']
-
-    def create(self, validated_data):
-        return Exercise.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.exercise_type = validated_data.get('exercise_type', instance.type)
-        instance.muscle = validated_data.get('muscle', instance.muscle)
-        instance.equipment = validated_data.get('equipment', instance.equipment)
-        instance.difficulty = validated_data.get('difficulty', instance.difficulty)
-        instance.instructions = validated_data.get('instructions', instance.instructions)
-        instance.save()
-        return instance
--- backend/beyondmd/views.py
+++ backend/beyondmd/views.py
@@ -4,15 +4,28 @@ from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from .models import Exercise, Reviewer
 from .serializers import ReviewerSerializer, ExerciseSerializer
+from django.core.cache import cache
 import requests
 import os
 from dotenv import load_dotenv
 
 
+# Load the API key from an environment variable
+load_dotenv()
+api_key = os.environ.get('API_KEY')
+
+
 @csrf_exempt
 @require_http_methods(["GET"])
 def get_exercises(request):
-    exercises = Exercise.objects.all()
+    # Check if the exercises are cached
+    exercises = cache.get('exercises')
+    if not exercises:
+        # If not, fetch the exercises from the database
+        exercises = Exercise.objects.all()
+        # Cache the exercises for future requests
+        cache.set('exercises', exercises)
+    # Serialize the exercises
     serializer = ExerciseSerializer(exercises, many=True)
     return JsonResponse(serializer.data, safe=False)
 
@@ -20,14 +33,13 @@ def get_exercises(request):
 @csrf_exempt
 @require_http_methods(["POST"])
 def save_exercise_from_api(request):
-    load_dotenv()
-    api_key = os.environ.get('API_KEY')
-
     exercise_type = ['cardio', 'olympic_weightlifting', 'plyometrics', 'powerlifting', 'strength', 'stretching',
                      'strongman']
 
-    for e in exercise_type:
+    # Create a list to store the new exercises
+    new_exercises = []
 
+    for e in exercise_type:
         offset = 0
         while True:
             response = requests.get('https://api.api-ninjas.com/v1/exercises',
@@ -41,7 +53,7 @@ def save_exercise_from_api(request):
 
             for data in datas:
                 # Check if the data already exists in the database, if not create
-                Exercise.objects.get_or_create(
+                exercise, created = Exercise.objects.get_or_create(
                     name=data.get("name", "default value"),
                     exercise_type=data.get("type", "default value"),
                     muscle=data.get("muscle", "default value"),
@@ -49,9 +61,18 @@ def save_exercise_from_api(request):
                     difficulty=data.get("difficulty", "default value"),
                     instructions=data.get("instructions", "default value")
                 )
+                if created:
+                    # If the exercise is new, add it to the list
+                    new_exercises.append(exercise)
 
             # Update the offset value by 10 to get the next set of results
             offset += 10
+
+    # Use the `bulk_create` method to efficiently insert multiple records into the database
+    Exercise.objects.bulk_create(new_exercises)
+    # Clear the cache to reflect the updated list of exercises
+    cache.delete('exercises')
+    # Display a success message to the user
     return HttpResponse("Exercises fetched successfully")
 
 
